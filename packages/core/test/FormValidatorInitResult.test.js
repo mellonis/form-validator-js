3083cb25d2ff01c3b8a1c2d231be2f4224b6790b
mellonis <mellonis@yandex.ru>
2019-08-05 17:40:23 +0500

    travis

--- packages/core/test/FormValidatorInitResult.test.js
+++ packages/core/test/FormValidatorInitResult.test.js
@@ -12,7 +12,7 @@ describe('FormValidatorInitResult', () => {
       // eslint-disable-next-line no-new
       new FormValidatorInitResult({});
     })
-      .toThrowError(/is not iterable$/);
+      .toThrowError(/is not iterable/);
 
     expect(new FormValidatorInitResult({
       observableElementList: [],
